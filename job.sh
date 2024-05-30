4a5416de07e3c98022aebb4c1b890ba241b83b0f
fffeelipe <lfepiar@unal.edu.co>
2024-05-30 18:24:17 +0200

    fixes

--- job.sh
+++ job.sh
@@ -4,5 +4,8 @@ export NNODES=2
 #SBATCH -N $NNODES # total number of nodes
 #SBATCH --time=00:05:00 # Max execution time
 #SBATCH --cpus-per-task=2
+#SBATCH --output stdout.out
+#SBATCH --error error.out
+
 
 mpirun -n 2 singularity exec ./matrixMul.sif /opt/main
