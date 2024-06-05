d98062aef9534264bcee83f7b87a08f5da27f2ca
mehdi.ghiasipour@mail.polimi.it <mehdi.ghiasipour@mail.polimi.it>
2024-06-05 14:38:44 +0200

    fix

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export NNODES=2
 #SBATCH --job-name singularity-mpi
-#SBATCH -N $NNODES # total number of nodes
+#SBATCH --nodes=1 # total number of nodes
+#SBATCH --ntasks=2
 #SBATCH --time=00:05:00 # Max execution time
 #SBATCH --cpus-per-task=2
 #SBATCH --output stdout.out
@@ -10,4 +10,4 @@ export NNODES=2
 export TMPDIR=$HOME/tmp
 mkdir -p $TMPDIR
 
-singularity exec matrixMul.sif bash -c "export OMPI_MCA_tempdir_base=$TMPDIR && mpirun -np 2 /opt/main"
+singularity exec --bind $TMPDIR:$TMPDIR matrixMul.sif bash -c "export OMPI_MCA_tempdir_base=$TMPDIR && mpirun -np 2 /opt/main" 
